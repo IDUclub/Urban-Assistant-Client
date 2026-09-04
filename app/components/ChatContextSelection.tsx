a5d1c90a49abb0ff218e25d2b0dc8cfd0bb4951a
DanilZmievskiy <danil.zmievskiy@gmail.com>
2026-09-04 18:59:57 +0300

    Revert "Revert "Проверить слои ПЗЗ""
    This reverts commit 4bad15e512608cd877da8139f0e7387664be04a4.

--- app/components/ChatContextSelection.tsx
+++ app/components/ChatContextSelection.tsx
@@ -18,7 +18,7 @@ import CreateScenarioModal from "@components/CreateScenarioModal";
 const CHAT_SERVICES = [
   "Зоны ограничений",
   "Проверка объектов по ПЗЗ",
-  "Сгенерировать застройку",
+  "Генерация застройки",
   "Генерация функционального зонирования",
   "Обеспеченность",
   "Проверка ВРИ",
@@ -186,7 +186,7 @@ const ChatContextSelection = observer(() => {
         ? CHAT_SERVICES.filter((service) => (
             service === "Проверка объектов по ПЗЗ"
             || service === "Проверка ВРИ"
-            || service === "Сгенерировать застройку"
+            || service === "Генерация застройки"
             || service === "Генерация функционального зонирования"
         ))
         : CHAT_SERVICES.filter((service) => (
